3e223b3c148204b65ed046ac35c581c10990ce51
mhmdNoman <mhmdnoman01@gmail.com>
2023-08-29 10:01:41 +0600

    search added

--- Mobile store api project/script.js
+++ Mobile store api project/script.js
@@ -1,6 +1,6 @@
-const loadPhone = async () => {
+const loadPhone = async (searchText) => {
   const res = await fetch(
-    "https://openapi.programming-hero.com/api/phones?search=iphone"
+    `https://openapi.programming-hero.com/api/phones?search=${searchText}`,
   );
   const data = await res.json();
   const phones = data.data;
@@ -10,13 +10,23 @@ const loadPhone = async () => {
 const phoneContainer = document.getElementById("phone-container");
 
 const displayPhones = (phones) => {
+  // display show all button if there are more than 9 phones
+  const showAll = document.getElementById("show-all");
+  if (phones.length > 9) {
+    showAll.classList.remove("hidden");
+  } else {
+    showAll.classList.add("hidden");
+  }
+
+  // display only first 9 phones
+  phones = phones.slice(0, 9);
+
   phones.forEach((phone) => {
-    console.log(phone);
     const phoneCard = document.createElement("div");
     phoneCard.innerHTML = `
     <div class="border flex flex-col items-center max-w-xs px-4 py-5 gap-2 rounded-md text-[#403F3F]">
     <img src="${phone.image}" alt="" />
-    <h2 class="text-2xl font-bold">Iphone 13 Pro Max</h2>
+    <h2 class="text-2xl font-bold">${phone.phone_name}</h2>
     <p class="text-[#706F6F]">
       There are many variations of passages of available, but the majority
       have suffered
@@ -28,8 +38,21 @@ const displayPhones = (phones) => {
   </div>
     `;
 
-    phoneContainer.appendChild(phoneCard)
+    phoneContainer.appendChild(phoneCard);
   });
 };
 
-loadPhone();
+// Handle Search
+const handleSearch = () => {
+  phoneContainer.innerHTML = "";
+  const searchInput = document.getElementById("seacrch-input");
+  const searchText = searchInput.value;
+  loadPhone(searchText);
+
+  searchInput.value = "";
+};
+
+// Handle Show all
+const handleShowAll = () => {
+  handleSearch();
+};
